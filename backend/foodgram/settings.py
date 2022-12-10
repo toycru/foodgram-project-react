0f8b33efb855205bbda33dc1bdbc2bcf94d8ed87
Дмитрий Пошехонов <toycru@ya.ru>
2022-12-11 00:42:52 +0300

    Edit Docker-compose and settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -75,8 +75,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', default='5432')
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default=5432)
     }
 }
 
